9be27e21380124d50273f95d5f1a8d9b31fcac3c
soumyaranjansaamal <soumyaranjansaamal@gmail.com>
2025-11-25 14:33:04 +0530

    cleanup: remove build artifacts and backups from repo

--- src-backup-1763963138/main/java/com/lms/lms_backend/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lms.lms_backend.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
